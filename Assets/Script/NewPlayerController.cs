e5cc6ef312f6e753ab05da2b708c0ffbed22ce5c
gzhao01 <1079756007@qq.com>
2021-02-24 13:17:12 +0800

    2021/2/24

---
+++ Assets/Script/NewPlayerController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayerController : MonoBehaviour
+{
+    private Animator anim;
+    private Collider2D coll;
+    private Rigidbody2D rb;
+
+    public float speed, jumpForce;
+    public Transform groundCheck;
+    public LayerMask ground;
+
+    private bool isJump, isGround,jumpPressed;
+    private int jumpCount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            jumpPressed = true;
+        }
+        //left dash
+        if(Input.GetButtonDown("Dash") && Input.GetButtonDown("Left") && !isDash)
+        {
+            dashDirection = 1;
+            isDash = true;
+            dashTime = dashTimeRecord;
+        }
+        //right dash
+        if (Input.GetButtonDown("Dash") && Input.GetButtonDown("Right") && !isDash)
+        {
+            dashDirection = 2;
+            isDash = true;
+            dashTime = dashTimeRecord;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGround = Physics2D.OverlapCircle(groundCheck.position,0.1f,ground);
+        groundMovement();
+        jump();
+        switchAnim();
+    }
+
+    void groundMovement()
+    {
+        float rawMove = Input.GetAxisRaw("Horizontal");
+        //移动
+        rb.velocity = new Vector2(rawMove*speed, rb.velocity.y);
+        //朝向
+        if (rawMove != 0)
+        {
+            transform.localScale = new Vector3(rawMove,1,1);
+        }
+        
+    }
+
+    void jump()
+    {
+        //每次在地面上
+        if (isGround)
+        {
+            jumpCount = 2;
+            isJump = false;
+        }
+        //一段跳(在地面或者空中下落的时候)
+        if (jumpPressed && jumpCount>0)
+        {
+            isJump = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+        //二段跳
+        else if (jumpPressed && isJump && jumpCount > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+    }
+
+    void switchAnim()
+    {
+        anim.SetFloat("run", Mathf.Abs(rb.velocity.x));
+
+        //falling -> idle
+        if (isGround)
+        {
+            anim.SetBool("falling", false);
+            anim.SetBool("idle", true);
+        }
+        //upward movement
+        if(!isGround && rb.velocity.y>0)
+        {
+            anim.SetBool("jumping",true);
+        }
+        //falling
+        if(!isGround &&rb.velocity.y<0)
+        {
+            anim.SetBool("falling", true);
+            anim.SetBool("jumping", false);
+        }
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    public float speed;
+    public float jumpForce;
+    public LayerMask ground;
+    public Collider2D coll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+        switchAnim();
+    }
+
+    void Move()
+    {
+        //get from -1.0~1.0
+        //walk
+        float horizontalMove = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontalMove * speed * Time.deltaTime, rb.velocity.y);
+        //get -1,0,1
+        //turn direction
+        float faceDirection = Input.GetAxisRaw("Horizontal");
+        anim.SetFloat("run",Mathf.Abs(faceDirection));
+        if(faceDirection != 0)
+        {
+            transform.localScale = new Vector3(faceDirection,1,1);
+        }
+        //jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
+            anim.SetBool("jumping", true);
+        }
+    }
+
+    void switchAnim()
+    {
+        if (anim.GetBool("jumping"))
+        {
+            if(rb.velocity.y < 0)
+            {
+                anim.SetBool("jumping", false);
+                anim.SetBool("falling", true);
+            }
+        }
+        else if (coll.IsTouchingLayers(ground))
+        {
+            anim.SetBool("falling", false);
+            anim.SetBool("idle",true);
+        }
+    }
+}
